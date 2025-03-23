150eaa80ecaef4f155b711846986b6a0b3429215
BlueDev <154534818+Diorite2006@users.noreply.github.com>
2025-03-23 10:35:36 +0700

    update navbar

--- form.php
+++ form.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>FROM</title>
+    <?php include('include/header.php'); ?>
+    <?php include('include/config.php'); ?>
 </head>
 <body>
-    
+    <?php include('include/navbar.php'); ?>
 </body>
 </html>
