de391ac346dbeeb2120b8619bd6c6d49850e21c6
_Nattapat.05 <natota4801@gmail.com>
2025-03-23 11:12:40 +0700

    success index

--- index.php
+++ index.php
@@ -31,29 +31,33 @@
         <hr>
 
         <?php
-        $date_id = $_GET['id'];
-        $sql = "SELECT * FROM `times` WHERE date_id = '$date_id'";
-        $result = $conn->query($sql);
-        while ($row = $result->fetch_array()) {
+        $date_id = isset($_GET['id']) ? $_GET['id'] : null;
+        if ($date_id) {
+            $sql = "SELECT * FROM `times` WHERE date_id = '$date_id'";
+            $result = $conn->query($sql);
+            while ($row = $result->fetch_array()) {
         ?>
-            <a href="" class="text-normal">
-                <div class="card my-2">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <h4><?php echo $row['time']; ?></h4>
-                                <h6 class="text-muted">Ws_Hospital</h6>
-                            </div>
-                            <div class="col-md-6 my-auto">
-                                <h4 class="float-end">status</h4>
+                <a href="form.php?time_id=<?php echo $row['id']; ?>" class="text-normal">
+                    <div class="card my-2 card-hover">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <h4><?php echo $row['time']; ?></h4>
+                                    <h6 class="text-muted">Ws_Hospital</h6>
+                                </div>
+                                <div class="col-md-6 my-auto">
+                                    <h4 class="float-end">status</h4>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </a>
-        <?php
-        }
-        ?>
+                </a>
+            <?php }
+        } else { ?>
+            <div class="alert alert-primary text-center" role="alert">
+                - กรุณาเลือกวันที่ ที่ต้องการ -
+            </div>
+        <?php } ?>
 
     </div>
 
