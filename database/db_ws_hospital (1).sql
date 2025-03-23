96a1024379a7d50fd557dcaac5dcbd86debeace8
BlueDev <154534818+Diorite2006@users.noreply.github.com>
2025-03-23 10:46:13 +0700

    sql update

--- database/db_ws_hospital (1).sql
+++ database/db_ws_hospital (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 23, 2025 at 04:38 AM
+-- Generation Time: Mar 23, 2025 at 04:45 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -30,7 +30,6 @@ SET time_zone = "+00:00";
 CREATE TABLE `dates` (
   `id` int(10) UNSIGNED NOT NULL,
   `date` date NOT NULL,
-  `time` varchar(100) NOT NULL,
   `status` tinyint(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -38,21 +37,14 @@ CREATE TABLE `dates` (
 -- Dumping data for table `dates`
 --
 
-INSERT INTO `dates` (`id`, `date`, `time`, `status`) VALUES
-(1, '2025-03-23', '08:00-12:00', 1),
-(2, '2025-03-23', '13:00-16:00', 0),
-(3, '2025-03-24', '08:00-12:00', 0),
-(4, '2025-03-24', '13:00-16:00', 1),
-(5, '2025-03-25', '08:00-12:00', 1),
-(6, '2025-03-25', '13:00-16:00', 0),
-(7, '2025-03-26', '08:00-12:00', 1),
-(8, '2025-03-26', '13:00-16:00', 0),
-(9, '2025-03-27', '08:00-12:00', 1),
-(10, '2025-03-27', '13:00-16:00', 1),
-(11, '2025-03-28', '08:00-12:00', 0),
-(12, '2025-03-28', '13:00-16:00', 1),
-(13, '2025-03-29', '08:00-12:00', 1),
-(14, '2025-03-29', '13:00-16:00', 0);
+INSERT INTO `dates` (`id`, `date`, `status`) VALUES
+(1, '2025-03-23', 1),
+(3, '2025-03-24', 0),
+(5, '2025-03-25', 1),
+(8, '2025-03-26', 0),
+(10, '2025-03-27', 1),
+(12, '2025-03-28', 1),
+(14, '2025-03-29', 0);
 
 -- --------------------------------------------------------
 
@@ -76,6 +68,38 @@ CREATE TABLE `reservations` (
 INSERT INTO `reservations` (`id`, `f_name`, `l_name`, `telephone`, `user_id`, `date_id`) VALUES
 (1, 'john', 'than', '098235123', '1209000251181', 1);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `times`
+--
+
+CREATE TABLE `times` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `time` varchar(50) NOT NULL,
+  `date_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `times`
+--
+
+INSERT INTO `times` (`id`, `time`, `date_id`) VALUES
+(1, '08:00-12:00', 1),
+(2, '13:00-16:00', 1),
+(3, '08:00-12:00', 3),
+(4, '13:00-16:00', 3),
+(5, '08:00-12:00', 5),
+(6, '13:00-16:00', 5),
+(7, '08:00-12:00', 8),
+(8, '13:00-16:00', 8),
+(9, '08:00-12:00', 10),
+(10, '13:00-16:00', 10),
+(11, '08:00-12:00', 12),
+(12, '13:00-16:00', 12),
+(13, '08:00-12:00', 14),
+(14, '13:00-16:00', 14);
+
 --
 -- Indexes for dumped tables
 --
@@ -95,6 +119,13 @@ ALTER TABLE `reservations`
   ADD KEY `date_id` (`date_id`);
 
 --
+-- Indexes for table `times`
+--
+ALTER TABLE `times`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `date_id` (`date_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -111,6 +142,12 @@ ALTER TABLE `reservations`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `times`
+--
+ALTER TABLE `times`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
 -- Constraints for dumped tables
 --
 
@@ -119,6 +156,12 @@ ALTER TABLE `reservations`
 --
 ALTER TABLE `reservations`
   ADD CONSTRAINT `reservations_ibfk_1` FOREIGN KEY (`date_id`) REFERENCES `dates` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `times`
+--
+ALTER TABLE `times`
+  ADD CONSTRAINT `times_ibfk_1` FOREIGN KEY (`date_id`) REFERENCES `dates` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- form.php
+++ form.php
@@ -13,10 +13,25 @@
     <div class="container py-5">
         <div class="rounded shadow-sm p-3">
             <h1 class="text-center">FROM</h1>
-            <form action="">
-                <div class="form-group">
-                    <label for=""></label>
-                    <input type="text"  class="form-control">
+            <form action="" method="post">
+                <div class="form-group mb-3">
+                    <label for="">First Name</label>
+                    <input type="text"  class="form-control" name="f_name" required>
+                </div>
+                <div class="form-group mb-3">
+                    <label for="">Last Name</label>
+                    <input type="text"  class="form-control" name="l_name" required>
+                </div>
+                <div class="form-group mb-3">
+                    <label for="">Telephone</label>
+                    <input type="text"  class="form-control" name="telephone" required>
+                </div>
+                <div class="form-group mb-3">
+                    <label for="">ID Card</label>
+                    <input type="text"  class="form-control" name="user_id" required>
+                </div>
+                <div class="form-group mb-3">
+                    <button type="submit" class="btn btn-primary">Reserve</button>
                 </div>
             </form>
         </div>
