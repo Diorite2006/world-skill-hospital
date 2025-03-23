6e7a249a85bc015245147cdbbec180ef990e285c
BlueDev <154534818+Diorite2006@users.noreply.github.com>
2025-03-23 10:58:24 +0700

    update frontend

--- form.php
+++ form.php
@@ -11,29 +11,31 @@
     <?php include('include/navbar.php'); ?>
 
     <div class="container py-5">
-        <div class="rounded shadow-sm p-3">
+        <div class="rounded shadow-sm p-3 bg-white">
             <h1 class="text-center">FROM</h1>
-            <form action="" method="post">
-                <div class="form-group mb-3">
-                    <label for="">First Name</label>
-                    <input type="text"  class="form-control" name="f_name" required>
-                </div>
-                <div class="form-group mb-3">
-                    <label for="">Last Name</label>
-                    <input type="text"  class="form-control" name="l_name" required>
-                </div>
-                <div class="form-group mb-3">
-                    <label for="">Telephone</label>
-                    <input type="text"  class="form-control" name="telephone" required>
-                </div>
-                <div class="form-group mb-3">
-                    <label for="">ID Card</label>
-                    <input type="text"  class="form-control" name="user_id" required>
-                </div>
-                <div class="form-group mb-3">
-                    <button type="submit" class="btn btn-primary">Reserve</button>
-                </div>
-            </form>
+            <div class="d-flex justify-content-center">
+                <form action="" method="post" class="w-75">
+                    <div class="form-group mb-3">
+                        <label for="">First Name</label>
+                        <input type="text"  class="form-control" name="f_name" required>
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="">Last Name</label>
+                        <input type="text"  class="form-control" name="l_name" required>
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="">Telephone</label>
+                        <input type="text"  class="form-control" name="telephone" required>
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="">ID Card</label>
+                        <input type="text"  class="form-control" name="user_id" required>
+                    </div>
+                    <div class="form-group mb-3">
+                        <button type="submit" class="btn btn-primary">Reserve</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </body>
---
+++ queue_code.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Queue Success</title>
+    <?php include('include/header.php'); ?>
+    <?php include('include/config.php'); ?>
+</head>
+<body>
+    <?php include('include/navbar.php'); ?>
+
+    <div class="container py-5 d-flex justify-content-center">
+        <div class="rounded shadow-sm p-3 d-flex justify-content-center align-items-center w-50 bg-white">
+            <div class="queue">
+                <div class="status text-center">
+                    <h1 class="text-success">0000</h1>
+                    <p class="text-muted    ">The reservation was completed.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
