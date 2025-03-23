2ebc55c97f303401678d7675b7962a3767f0c8c2
BlueDev <154534818+Diorite2006@users.noreply.github.com>
2025-03-23 11:16:38 +0700

    insert

--- form.php
+++ form.php
@@ -10,11 +10,25 @@
 <body>
     <?php include('include/navbar.php'); ?>
 
+    <?php
+        $time = $_GET['time_id'];
+
+        $sql = "select * from times as t inner join dates as d on d.id = t.date_id where t.id = ".$time;
+        $result = $conn->query($sql);
+        $get = $result->fetch_array();
+    ?>
+
     <div class="container py-5">
         <div class="rounded shadow-sm p-3 bg-white">
             <h1 class="text-center">FROM</h1>
             <div class="d-flex justify-content-center">
                 <form action="" method="post" class="w-75">
+                    <input type="hidden"  class="form-control" name="date_id" value="<?=$get['date_id']?>" required>
+                    <div class="time d-flex justify-content-center gap-3">
+                        <h4>Date: <?php echo $get['date'] ?></h4>
+                        <span>|</span>
+                        <h4>Time: <?php echo $get['time'] ?></h4>
+                    </div>
                     <div class="form-group mb-3">
                         <label for="">First Name</label>
                         <input type="text"  class="form-control" name="f_name" required>
